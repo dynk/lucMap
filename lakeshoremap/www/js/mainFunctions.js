f65cef89db84ae0774f2a582b10e342e7692392a
Raphael Salvador <rsalvador1@luc.edu>
2016-04-10 12:09:32 -0500

    Function "Where am I?" removed

--- lakeshoremap/www/js/mainFunctions.js
+++ lakeshoremap/www/js/mainFunctions.js
@@ -220,19 +220,6 @@ function toggleMarkers() {
     $(document).on("pageinit", "#homePage", function (e) {
         //prevent any bound defaults
         e.preventDefault();
-
-        //loader function after deviceready event returns
-        function onDeviceReady() {
-
-            //handle button press for geolocation
-            $("#getLocation").on("tap", function (e) {
-                e.preventDefault();
-                getLocation();
-                $("#leftpanel").panel("close");
-            })
-        }
-        //as deviceready returns load onDeviceReady()
-        $(document).on("deviceready", onDeviceReady);
     });
 })();
 
